9005f828371ae5863a56ecf8f48fa4f8e6f0d211
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-01-14 11:55:01 +0100

    BDD crée

--- app/controllers/bookmarksController.js
+++ app/controllers/bookmarksController.js
@@ -1,14 +1,11 @@
-import path from 'node:path';
 
 const bookmarksController = {
 
   // méthode pour afficher les favoris
-  bookmarksPage(request, response) {
-    // A remplacer par le rendu d'un fichier EJS
-    response.sendFile('favoris', {
-      root: path.join(import.meta.dirname, '../../public'),
-    });
-  }
+  bookmarksPage(req, res) {
+   
+    res.render('favoris');
+  },
 
 };
 
--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,19 +1,19 @@
-import path from 'node:path';
 
 const mainController = {
 
   // méthode pour la page d'accueil
-  homePage(request, response) {
-    // Remplacer le response.sendFile par le rendu d'un fichier EJS
-    response.render('accueil')
+  homePage(req, res) {
+    
+    res.render('accueil')
     },
   
-
   // méthode pour la page article
-  articlePage(request, response) {
-    // Remplacer le response.sendFile par le rendu d'un fichier EJS
-    response.render('article')
+  articlePage(req, res) {
+    
+    res.render('article')
     },
+
+
   }
 
 

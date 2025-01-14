3c0a7e72d4756ef3ddd6afaeb2f5839ed64cb70f
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-01-14 11:24:35 +0100

    étape 1 et 2 ok, problème précédent résolu

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -5,19 +5,17 @@ const mainController = {
   // méthode pour la page d'accueil
   homePage(request, response) {
     // Remplacer le response.sendFile par le rendu d'un fichier EJS
-    response.sendFile('accueil', {
-      root: path.join(import.meta.dirname, '../../public'),
-    });
-  },
+    response.render('accueil')
+    },
+  
 
   // méthode pour la page article
   articlePage(request, response) {
     // Remplacer le response.sendFile par le rendu d'un fichier EJS
-    response.sendFile('article', {
-      root: path.join(import.meta.dirname, '../../public'),
-    });
+    response.render('article')
+    },
   }
-};
+
 
 
 export default mainController;
